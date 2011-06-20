0bfd20377c196cd4ba80739c1d4717642678651a
U-IPO\nevans <nevans@nevans.IPO.local>
2011-06-20 14:22:46 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ebdd8320dccdd50b255dfcce07515d31ae725457e948603e9508a414b0c9c1465647ae1e276efc8b4b037301a11a265b568cd74594a8b27a90a0288b5d3a4d57'
